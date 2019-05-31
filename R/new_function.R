cf8cefb765821c1aa038a8ddaeef0eeefb654564
mollywilliams <mollylegate@gmail.com>
2019-05-31 10:48:26 -0700

    documentation updates

---
+++ R/new_function.R
@@ -0,0 +1,15 @@
+#' Gary is cute, but how cute?
+#'
+#' This is how you find out how cute Gary is
+#' @param t is he asking for a treat?
+#' @param h did he just get a haircut?
+#' @param a are other animals around?
+#' @param b baseline cuteness
+#' @return cuteness
+#'
+#'
+
+gary = function(t, h, a, b=100) {
+P = t*(h^3)*a + b
+return(C)
+}
---
+++ R/wm.R
@@ -0,0 +1,15 @@
+#' Average Heights and Weights for American Women
+#'
+#' This data set gives the average heights and weights for American women aged 30–39.
+#'
+#' @format A data frame with 15 observatins on 2 variables
+#' \itemize{
+#' \item height (inches)
+#' \item weight (lbs)
+#' }
+#' @details The data set appears to have been taken from the American Society of Actuaries Build and Blood Pressure Study for some (unknown to us) earlier year.
+#' @source The World Almanac and Book of Facts, 1975.
+#' @references McNeil, D. R. (1977) Interactive Data Analysis. Wiley.
+#' @examples mean(wm$height)
+#'
+"wm"
